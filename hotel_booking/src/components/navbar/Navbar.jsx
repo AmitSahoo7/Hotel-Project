89ab064c9a0d08f70df4b94e677696aac40a2702
AmitSahoo7 <amit.sahoo757@gmail.com>
2025-04-15 22:09:43 +0530

    navbar updated

--- hotel_booking/src/components/navbar/Navbar.jsx
+++ hotel_booking/src/components/navbar/Navbar.jsx
@@ -1,20 +1,17 @@
-import "./navbar.css";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import "./navbar.css"
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <Link to="/" className="logo"> {/* Wrap the logo with Link */}
-          <span>Bookify</span>
-        </Link>
+        <span className="logo">Bookify</span>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
