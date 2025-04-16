33a9f548f5e487528f653c31f6339babad348fc3
Amrit689 <capamrit007@gmail.com>
2025-04-16 21:17:22 +0530

    search item

--- hotel_booking/src/App.jsx
+++ hotel_booking/src/App.jsx
@@ -1,6 +1,8 @@
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Home from "./pages/home/Home";
 import HotelDetail from "./pages/hotelDetail/HotelDetail"; // Import the HotelDetail component
+import List from "./pages/list/List";
+
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/hotel/:id" element={<HotelDetail />} /> {/* Add this route */}
+        <Route path="/hotels" element={<List/>}/>
       </Routes>
     </BrowserRouter>
   );
--- hotel_booking/src/components/navbar/Navbar.jsx
+++ hotel_booking/src/components/navbar/Navbar.jsx
@@ -1,17 +1,20 @@
-import "./navbar.css"
+import { Link } from "react-router-dom";
+import "./navbar.css";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">Bookify</span>
+        <Link to="/" className="logo">
+          Bookify
+        </Link>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
